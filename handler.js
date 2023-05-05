e0554904000d9cb230ddb828815547bab1bf0d23
shirleen <shirleenoey@gmail.com>
2023-05-05 18:28:33 +0700

    commit initial files

---
+++ handler.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const emojis = [
+  '🙂','😊','😁','😄','😆','🥰','🤩','😍','😎','🥳'
+];
+
+module.exports.rank = async (event) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emoji[rank >= emojis.length ? emojis.length - 1 : rank]
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: rankEmoji,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
